119ba5c2537633f95a521a3d7e8e9f57457e5890
Estevam Rodrigues <estevam.mrodrigues@gmail.com>
2019-01-24 02:16:52 -0200

    Auth with jwt

--- app/api/login.js
+++ app/api/login.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../config/config.json')[env];
 
 module.exports = (app, db) => {
   app.post( "/login", (req, res) =>
@@ -8,13 +11,19 @@ module.exports = (app, db) => {
       }
     }).then(function (login) {
       if (!login) {
-        res.json('Invalid params');
+        res.json({message: "Invalid params"});
       } else {
         bcrypt.compare(req.body.password, login.password, function (err, result) {
           if (result == true) {
-            res.json(login);
+            var token = jwt.sign(login.toJSON(), config.jwt, {
+              expiresIn: 1440
+            });
+            res.json({ 
+              data: login,
+              token: token
+            });
           } else {
-            res.json('Incorrect password');
+            res.json({message: "Incorrect password"});
           }
         })
       }
@@ -28,4 +37,24 @@ module.exports = (app, db) => {
       password: bcrypt.hashSync(req.body.password, 10)
     }).then( (result) => res.json(result) )
   );
+
+  app.use(function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    
+    if (token) {
+      jwt.verify(token, config.jwt, function(err, decoded) {      
+        if (err) {
+          return res.json({success: false, message: 'Token is invalid!'});    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      return res.status(403).send({ 
+        success: false, 
+        message: 'Token is not provided.' 
+      });       
+    }
+  });
 }
